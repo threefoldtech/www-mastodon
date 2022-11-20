80bf1d7d255fdcfc95f6e6143a35ea299690d211
MohamedElmdary <engm5081@gmail.com>
2022-11-20 13:04:56 +0200

    Add required packages

---
+++ src/Mastodon.svelte
@@ -0,0 +1,11 @@
+<svelte:options tag="tf-mastodon" />
+
+<script context="module" lang="ts">
+  console.log("context");
+</script>
+
+<script lang="ts">
+  console.log("mastodon");
+</script>
+
+<p>mastodon</p>
