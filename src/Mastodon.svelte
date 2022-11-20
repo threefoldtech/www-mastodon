8cc9b5b8c9df0aa378cdb4ffd93eff806bed4c7b
MohamedElmdary <engm5081@gmail.com>
2022-11-20 14:11:55 +0200

    Add credentials page

--- src/Mastodon.svelte
+++ src/Mastodon.svelte
@@ -1,11 +1,82 @@
 <svelte:options tag="tf-mastodon" />
 
 <script context="module" lang="ts">
-  console.log("context");
+  import { Tabs, Input, CheckBox } from "tf-svelte-bulma-wc";
+  import { fb, validators } from "tf-svelte-rx-forms";
+  import { validateMnemonic } from "bip39";
 </script>
 
 <script lang="ts">
-  console.log("mastodon");
+  let active = "credentials";
+
+  const enableSShKey = fb.control(false);
+  const loginForm = fb.group({
+    mnemonics: [
+      "",
+      [
+        validators.required("Mnemonics is required."),
+        (ctrl) => {
+          if (!validateMnemonic(ctrl.value)) {
+            return { message: "Mnemonic doesn't seem to be valid." };
+          }
+        },
+      ],
+    ],
+    sshKey: ["", [validators.required("Public SSH Key is required.")]],
+  });
+
+  $: enableSShKey$ = $enableSShKey;
+  $: loginForm$ = $loginForm;
+  $: showConfigs =
+    loginForm$.valid ||
+    (loginForm$.value.mnemonics.valid && !enableSShKey$.value);
 </script>
 
-<p>mastodon</p>
+<b-box>
+  <Tabs
+    bind:active
+    tabs={[
+      { id: "credentials", label: "Credentials" },
+      ...(showConfigs
+        ? [
+            { id: "basic", label: "Basic" },
+            { id: "advanced", label: "Advanced" },
+          ]
+        : []),
+    ]}
+  />
+
+  <section style:display={active === "credentials" ? "initial" : "none"}>
+    <Input
+      label="Mnemonics"
+      placeholder="Mnemonics"
+      controller={loginForm.get("mnemonics")}
+    />
+
+    <div class="is-flex">
+      <div style:width="100%">
+        <Input
+          label="Public SSH Key"
+          type="textarea"
+          placeholder="Your public SSH Key"
+          controller={loginForm.get("sshKey")}
+          disabled={!enableSShKey$.value}
+          validation={enableSShKey$.value}
+        />
+      </div>
+
+      <div class="mt-6 ml-2">
+        <b-tooltip
+          tooltip="On disable the deployed solutions'll be inaccessible."
+          flow="left"
+        >
+          <CheckBox controller={enableSShKey} />
+        </b-tooltip>
+      </div>
+    </div>
+  </section>
+
+  <section style:display={active === "basic" ? "initial" : "none"}>
+    basic
+  </section>
+</b-box>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +12,20 @@ export default defineConfig({
       },
     }),
   ],
+
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
 });
